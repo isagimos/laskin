2e02a8045f5347f2083c7d11ad9fb59eb057ebbb
Ismo <152513223+agismos@users.noreply.github.com>
2025-03-24 10:38:04 +0200

    check that user can only input a sensible calculation

--- src/services/calculator_logic.py
+++ src/services/calculator_logic.py
@@ -4,12 +4,43 @@ import sympy as sp
 class CalculatorLogic:
     def __init__(self):
         self._root = self
+
+        self._operators = ["+", "-", "*", "/"]
     
     def handle_click(self, entry_value, button, username):
         if entry_value == "Virhe":
             entry_value = ""
             return button
         
+        # At first the user cannot click an operator or "=" symbol:
+        if entry_value == "":
+            if button in self._operators or button == "=":
+                return entry_value
+            
+        if button.isdigit():
+            if entry_value == "0":
+                return button
+            #if len(entry_value) == 1 and entry_value[-1] == "0":
+            try:
+                if entry_value[-1] == "0" and entry_value[-2] in self._operators:
+                    return entry_value[:-1] + button
+            except IndexError:
+                return entry_value + button
+        
+        if button in self._operators:
+            if len(entry_value) < 1:
+                return entry_value
+            else:
+                if entry_value[-1] in self._operators:
+                    return entry_value
+        
+        if button == ".":
+            try:
+                if entry_value[-1] == ".":
+                    return entry_value
+            except IndexError:
+                return button
+
 ### Tästä etenpäin ChatGPT:llä generoitua koodia riveillä, jotka merkitty #:
         if button == "=":
             try:                                                                #
