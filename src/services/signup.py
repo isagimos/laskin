f3900402d4931f7354b42bf17e00e5baa93b936d
Ismo <152513223+agismos@users.noreply.github.com>
2025-03-26 14:26:23 +0200

    check username and passowrd length

--- src/services/signup.py
+++ src/services/signup.py
@@ -15,12 +15,19 @@ class SignUp:
 
     def _create_account(self, username, password1, password2):
 
+        if not self._check_username_length(username):
+            return "Tunnuksen vähimmäispituus on 3 merkkiä"
+
         if self._check_if_unique(username):
             if password1 == password2:
+                if len(password1) < 8:
+                    return "Salasanan vähimmäispituus on 8 merkkiä"
                 self._add_username_and_password(username, password1)
                 return "Tunnus luotu"
             return "Salasanat eivät täsmää"
         return "Tunnus on jo käytössä"
+    
+
 
 
     def _check_if_unique(self, username):
@@ -51,4 +58,8 @@ class SignUp:
                 newuser = f"{username};{generate_password_hash(password)}"
                 f.write(newuser + "\n")
                 return True
-        
+    
+    def _check_username_length(self, username):
+        if len(username) < 3:
+            return False
+        return True
