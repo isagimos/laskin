77d2dda33b5c57277197bfce6c7cf8fcd0cfd460
Ismo <152513223+agismos@users.noreply.github.com>
2025-03-28 10:34:56 +0200

    lint code

--- src/services/calculator_logic.py
+++ src/services/calculator_logic.py
@@ -6,24 +6,24 @@ class CalculatorLogic:
         self._root = self
 
         self._operators = ["+", "-", "*", "/"]
-    
+
     def handle_click(self, entry_value, button, username):
         if button == "C":
             return ""
-        
+
         if entry_value == "Virhe":
             if button.isdigit():
                 return button
             return ""
-        
+
         if button == "<-":
             return entry_value[:-1]
-        
+
         # At first the user cannot click an operator or "=" symbol:
         if entry_value == "":
             if button in self._operators or button == "=":
                 return entry_value
-            
+
         if button.isdigit():
             if entry_value == "0":
                 return button
@@ -33,14 +33,13 @@ class CalculatorLogic:
                     return entry_value[:-1] + button
             except IndexError:
                 return entry_value + button
-        
+
         if button in self._operators:
             if len(entry_value) < 1:
                 return entry_value
-            else:
-                if entry_value[-1] in self._operators:
-                    return entry_value
-        
+            if entry_value[-1] in self._operators:
+                return entry_value
+
         if button == ".":
             try:
                 if entry_value[-1] == ".":
@@ -52,12 +51,12 @@ class CalculatorLogic:
         if button == "=":
             try:                                                                #
                 result = sp.simplify(entry_value)                               #
-                if result == sp.zoo or result == sp.nan or result == sp.oo or result == -sp.oo:     #
+                if result in {sp.zoo, sp.nan, sp.oo, -sp.oo}:                   #
                     result = "Virhe"                                            #
                                                                                 #
                 result = float(result)                                          #
-    
-            except:                                                             #
+
+            except (SyntaxError, ValueError):                                   #
                 result = "Virhe"                                                #
 
             file_path = os.path.join("data", "calculations.csv")
@@ -67,8 +66,8 @@ class CalculatorLogic:
 
             try:                                                                #
                 return f"{result:.10g}"                                         #
-            except:                                                             #
+            except ValueError:                                                  #
                 return result                                                   #
-            
+
         return entry_value + button
 ### ChatGPT:llä generoitu koodi päättyy
