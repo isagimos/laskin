058604a6500d1bebcc3e1c99df7ab5ad968c7386
Ismo <152513223+agismos@users.noreply.github.com>
2025-04-28 14:27:12 +0300

    docstring users_repository.py

--- src/repositories/users_repository.py
+++ src/repositories/users_repository.py
@@ -2,7 +2,16 @@ import os
 from werkzeug.security import check_password_hash, generate_password_hash
 
 class UsersRepository:
+    """Class for managing the users saved to csv file.
+    """
     def __init__(self, root):
+        """The constructor of the class. Defines the directory of the csv-file.
+
+        Args:
+            dir: Directory.
+            file: Name of the csv-file.
+            file_path: The path for the csv-file.
+        """
         self._root = root
 
         self.dir = "data"
@@ -13,8 +22,17 @@ class UsersRepository:
         os.makedirs(os.path.dirname(self.file_path), exist_ok=True)
         ### ChatGPT:llä generoitu koodi päättyy
 
-
     def check_username_and_password(self, username, password):
+        """Check if the given username and password match.
+
+        Args:
+            username: Username given by the user.
+            password: Password given by the user.
+
+        Returns:
+            If username and password match, return True.
+            If not, return an error message.
+        """
         try:
             file_path = os.path.join("data", "users.csv")
             with open(file_path, "r", encoding="utf-8") as f:
@@ -30,6 +48,15 @@ class UsersRepository:
         return "Väärä käyttäjätunnus tai salasana"
 
     def check_username(self, username):
+        """Check if the username entered by the user exists in the csv file.
+
+        Args:
+            username: Username given by the user.
+
+        Returns:
+            Returns False, if the entered username is already taken by somebody else.
+            Return True, if username is available.
+        """
         try:
             self.file_path = os.path.join(self.dir, self.file)
             with open(self.file_path, "r", encoding="utf-8") as f:
@@ -45,6 +72,15 @@ class UsersRepository:
             return True
 
     def add_username_and_password(self, username, password):
+        """Add newly created username and password to csv file.
+
+        Args:
+            username: Username entered by user.
+            password: Password entered by user.
+
+        Returns:
+            Return True after adding username and password to csv file.
+        """
         ### ChatGPT:llä generoitu koodi alkaa
         self.file_path = os.path.join("data", "users.csv")
         try:
